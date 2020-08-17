05dc91619e483c364cf99c16c91bf0e6eb742d09
ormus395 <ormus395@gmail.com>
2020-08-17 13:11:43 -0700

    Started on blog home feed

--- client/src/pages/BlogHome/index.js
+++ client/src/pages/BlogHome/index.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./BlogHome.css";
 
 const BlogHome = () => {
+  const [recentPosts, setRecentPosts] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+
+    fetch("/api/blog")
+      .then((response) => response.json())
+      .then((jsonResponse) => console.log(jsonResponse))
+      .catch((err) => {
+        setIsError(true);
+        console.log(err);
+      });
+  }, []);
+
   return (
-    <div>
-      <h3>Blog home feed</h3>
-    </div>
+    <main className="container">
+      <h3 className="blog-page-title">Recent Articles</h3>
+      <section className="articles"></section>
+    </main>
   );
 };
 
